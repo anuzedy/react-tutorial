a67b4ff5ad27cad4881b68143693e20beb844255
anuzedy <ksjqsr@gmail.com>
2020-12-12 23:11:23 +0900

    setInterval in rifecycle

--- 05/RSP.jsx
+++ 05/RSP.jsx
@@ -4,23 +4,52 @@ import React, { Component } from 'react';
 //              -> (setState/props 바뀔때 -> shouldComponentUpdate가 true면 -> render -> componentDidUpdate)
 //              -> 부도가 나를 없앴을 때 -> componentWillUnmount -> 소멸
 
+const rspCoords = {
+  바위: '0',
+  가위: '-147px',
+  보: '-284px',
+}
+
+const scores = {
+  승리: 1,
+  무승부: 0,
+  패배: -1,
+}
+
 class RSP extends Component {
   state = {
     result: '',
-    imgCoord: 0,
+    imgCoord: '0',
     score: 0,
   };
 
-  componentDidMount() { // 컴포넌트가 첫 렌더링된 후
+  interval;
 
+  componentDidMount() { // 컴포넌트가 첫 렌더링된 후, 여기에 비동기 요청을 많이 함
+    this.interval = setInterval(() => {
+      const {imgCoord} = this.state;
+      if(imgCoord === rspCoords.바위) {
+        this.setState({
+          imgCoord: rspCoords.가위,
+        });
+      } else if(imgCoord === rspCoords.가위) {
+        this.setState({
+          imgCoord: rspCoords.보,
+        });
+      } else if(imgCoord === rspCoords.보) {
+        this.setState({
+          imgCoord: rspCoords.바위,
+        });
+      }
+    }, 1000);
   }
 
   componentDidUpdate() {  // 리렌더링 후
 
   }
 
-  componentWillUnmount() {  // 컴포넌트가 제거되기 직전
-
+  componentWillUnmount() {  // 컴포넌트가 제거되기 직전, 비동기 요청 정리를 많이 함
+    clearInterval(this.interval);
   }
 
   render() {
