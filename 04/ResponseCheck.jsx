b1cf2c41387e6afa1ce3dc106998763fc7d05994
anuzedy <ksjqsr@gmail.com>
2020-12-12 20:56:27 +0900

    add setTimeout

--- 04/ResponseCheck.jsx
+++ 04/ResponseCheck.jsx
@@ -7,6 +7,42 @@ class ResponseCheck extends Component {
     result: [],
   };
 
+  timeout;
+  startTime;
+  endTime;
+
+  onClickScreen = () => {
+    const { state, message, result } = this.state;
+    if(state === 'waiting') {
+      this.setState({
+        state: 'ready',
+        message: '초록색이 되면 클릭하세요.',
+      });
+      this.timeout = setTimeout(() => {
+        this.setState({
+          state: 'now',
+          message: '지금 클릭',
+        });
+        this.startTime = new Date();
+      }, Math.floor(Math.random() * 1000) + 2000);
+    } else if(state === 'ready') {  // 성급하게 클릭
+      clearTimeout(this.timeout);
+      this.setState({
+        state: 'waiting',
+        message: '너무 성급하시군요! 초록색이 된 후에 클릭하세요.',
+      });
+    } else if(state === 'now') {  // 반응속도 체크
+      this.endTime = new Date();
+      this.setState((prevState) => {
+        return {
+          state: 'waiting',
+          result: [...prevState.result, this.endTime - this.startTime],
+          message: '클릭해서 시작하세요.',
+        }
+      });
+    }
+  };
+
   renderAverage = () => {
     const { result } = this.state;
     return this.state.result.length === 0 ? 
@@ -17,10 +53,12 @@ class ResponseCheck extends Component {
     return (
       <>
         <div id="screen" className={this.state.state} onClick={this.onClickScreen}>
-          {this.state.mesesage}
+          {this.state.message}
         </div>
         {this.renderAverage()}
       </>
     );
   }
 }
+
+export default ResponseCheck;
