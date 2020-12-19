b3918e414607b7c9efed098e8df56a9ba6861506
anuzedy <ksjqsr@gmail.com>
2020-12-19 22:54:55 +0900

    action dispatch

--- 07/Tr.jsx
+++ 07/Tr.jsx
@@ -1,10 +1,10 @@
 import React from 'react';
 import Td from './Td';
 
-const Tr = ({ rowData }) => {
+const Tr = ({ rowData, rowIndex, dispatch }) => {
   return (
     <tr>
-      {Array(rowData.length).fill().map((td) => (<Td>{''}</Td>))}
+      {Array(rowData.length).fill().map((td, i) => (<Td rowIndex={rowIndex} cellIndex={i} cellData={rowData[i]} dispatch={dispatch}>{''}</Td>))}
     </tr>
   );
 };
