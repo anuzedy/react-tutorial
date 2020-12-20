07258bbd3a0fceaebd90cc8ee3eaf8d85844d76b
anuzedy <ksjqsr@gmail.com>
2020-12-20 22:19:49 +0900

    table optimization

--- 07/Table.jsx
+++ 07/Table.jsx
@@ -1,10 +1,15 @@
-import React from 'react';
+import React, { useMemo } from 'react';
 import Tr from './Tr';
 
 const Table = ({ onClick, tableData, dispatch }) => {
   return (
     <table>
-      {Array(tableData.length).fill().map((tr, i) => (<Tr rowIndex={i} rowData={tableData[i]} dispatch={dispatch}>{''}</Tr>))}
+      {Array(tableData.length).fill().map((tr, i) => (
+        useMemo(
+          () => <Tr key={i} rowIndex={i} rowData={tableData[i]} dispatch={dispatch}>{''}</Tr>
+          , [tableData[i]]
+        )
+      ))}
     </table>
   );
 };
--- 07/Td.jsx
+++ 07/Td.jsx
@@ -1,7 +1,16 @@
-import React, { useCallback } from 'react';
+import React, { useCallback, useEffect, useRef, memo } from 'react';
 import { CLICK_CELL } from './TicTacToe';
 
-const Td = ({ rowIndex, cellIndex, cellData, dispatch }) => {
+const Td = memo(({ rowIndex, cellIndex, cellData, dispatch }) => {
+  console.log('td rendered');
+
+  const ref = useRef([]);
+  useEffect(() => {
+    console.log(rowIndex === ref.current[0], cellIndex === ref.current[1], cellData === ref.current[2], dispatch === ref.current[3]);
+    console.log(cellData, ref.current[2]);
+    ref.current = [rowIndex, cellIndex, cellData, dispatch];
+  }, [rowIndex, cellIndex, cellData, dispatch]);
+
   const onClickTd = useCallback(() => {
     console.log(rowIndex, cellIndex);
     if(cellData) {
@@ -13,6 +22,6 @@ const Td = ({ rowIndex, cellIndex, cellData, dispatch }) => {
   return (
     <td onClick={onClickTd}>{cellData}</td>
   );
-};
+});
 
 export default Td;
