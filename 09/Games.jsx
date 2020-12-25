fb83a984c3af49edf600973dfb1805ae5e33f397
anuzedy <ksjqsr@gmail.com>
2020-12-25 23:54:21 +0900

    BrowserRouter

--- 09/Games.jsx
+++ 09/Games.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter, HashRouter } from 'react-router-dom';
+import { BrowserRouter, HashRouter, Link, Route } from 'react-router-dom';
 import NumberBaseball from '../03/NumberBaseball';
 import RSP from '../05/RSP';
 import Lotto from '../06/Lotto';
@@ -7,6 +7,13 @@ import Lotto from '../06/Lotto';
 const Games = () => {
   return (
     <BrowserRouter>
+      <div>
+        <Link to="/number-baseball">숫자야구</Link>
+        &nbsp;
+        <Link to="/rock-scissors-paper">가위바위보</Link>
+        &nbsp;
+        <Link to="/lotto-generator">로또생성기</Link>
+      </div>
       <div>
         <Route path="/number-baseball" component={NumberBaseball} />
         <Route path="/rock-scissors-paper" component={RSP} />
