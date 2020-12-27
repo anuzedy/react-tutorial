cb4bd7cde3e0e3da41e2453ae3db1b961fe35933
anuzedy <ksjqsr@gmail.com>
2020-12-27 18:33:03 +0900

    match, history

--- 09/GameMatcher.jsx
+++ 09/GameMatcher.jsx
@@ -1,11 +1,22 @@
 import React, { Component } from 'react';
 import { withRouter } from 'react-router-dom';
+import NumberBaseball from '../03/NumberBaseball';
+import RSP from '../05/RSP';
+import Lotto from '../06/Lotto';
 
 class GameMatcher extends Component {
   render() {
-    console.log(this.props);
+    if(this.props.match.params.name === 'NumberBaseball') {
+      return <NumberBaseball />;
+    } else if(this.props.match.params.name === 'rock-scissors-paper') {
+      return <RSP />;
+    } else if(this.props.match.params.name === 'lotto-generator') {
+      return <Lotto />;
+    }
     return (
-      <div>게임매쳐</div>
+      <div>
+        일치하는 게임이 없습니다.
+      </div>
     );
   }
 }
