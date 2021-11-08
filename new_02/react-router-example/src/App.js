b6cbb515e31548a326e2fdc8a6b787b87e353d71
anuzedy <ksjqsr@gmail.com>
2021-11-08 23:39:12 +0900

    Links

--- new_02/react-router-example/src/App.js
+++ new_02/react-router-example/src/App.js
@@ -1,17 +1,32 @@
 import logo from "./logo.svg";
 import "./App.css";
-import { BrowserRouter, Route } from "react-router-dom";
+import { BrowserRouter, Redirect, Route, Switch } from "react-router-dom";
 import Home from "./pages/Home";
 import Profile from "./pages/Profile";
 import About from "./pages/About";
+import NotFound from "./pages/NotFound";
+import Links from "./components/Links";
+import NavLinks from "./components/NavLinks";
+import Login from "./pages/Login";
+
+const isLogin = false;
 
 function App() {
   return (
     <BrowserRouter>
-      <Route path="/" exact component={Home}></Route>
-      <Route path="/profile" exact component={Profile}></Route>
-      <Route path="/profile/:id" component={Profile}></Route>
-      <Route path="/about" component={About}></Route>
+      <Links />
+      <NavLinks />
+      <Switch>
+        <Route
+          path="/login"
+          render={() => (isLogin ? <Redirect to="/" /> : <Login />)}
+        />
+        <Route path="/profile/:id" component={Profile} />
+        <Route path="/profile" component={Profile} />
+        <Route path="/about" component={About} />
+        <Route path="/" exact component={Home} />
+        <Route component={NotFound} />
+      </Switch>
     </BrowserRouter>
   );
 }
