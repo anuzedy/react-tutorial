700d3a4775b7f2c3f050198b539ccf61199c3a0f
anuzedy <ksjqsr@gmail.com>
2021-11-19 23:32:05 +0900

    additional hooks

--- new_02/react-router-example/src/components/LoginButton.jsx
+++ new_02/react-router-example/src/components/LoginButton.jsx
@@ -1,10 +1,20 @@
-import { withRouter } from "react-router-dom";
+import { useHistory, withRouter } from "react-router-dom";
 
-export default withRouter(function LoginButton(props) {
+// export default withRouter(function LoginButton(props) {
+//   const login = () => {
+//     setTimeout(() => {
+//       props.history.push("/");
+//     }, 1000);
+//   };
+//   return <button onClick={login}>로그인하기</button>;
+// });
+
+export default function LoginButton() {
+  const history = useHistory();
   const login = () => {
     setTimeout(() => {
-      props.history.push("/");
+      history.push("/");
     }, 1000);
   };
   return <button onClick={login}>로그인하기</button>;
-});
+}
